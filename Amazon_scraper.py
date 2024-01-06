a9621b5d5c837612d42585d546da42f2e7dddf60
ckauber <ckauber17@gmail.com>
2024-01-06 15:34:17 -0500

    Create functions and clean code. End of brief Beautiful Soup experiment.

--- Amazon_scraper.py
+++ Amazon_scraper.py
@@ -9,28 +9,27 @@ from bs4 import BeautifulSoup           #data parsing library
 import time
 
 #Amazon blocked me so I have to use different website
-product_URL = 'https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html'
+URL = 'https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html'
 
 #The User Agent is a string of info that your browser sends to a web server as part of an HTTP request.
 #It helps the server (Amazon here) identify you and send content that is compatible with your browser. 
 headers={"accept-language": "en-US,en;q=0.9","accept-encoding": "gzip, deflate, br","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36","accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"}
 
+def print_title(product_URL):
+    page = requests.get(product_URL, headers=headers)
 
-#This line scrapes all the information from the amazon url provided above
-page = requests.get(product_URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser') #Parses through all of the amazon HTML data
 
-f = open("html_code.txt", "w")
-f.write(page.text)
-f.close()
-print(page.text)
+    title = soup.find("h1").text.strip()
+    print("The title is", title)
 
-#Parses through all of the amazon HTML data
-soup = BeautifulSoup(page.content, 'html.parser')
+def check_price(product_URL):
+    page = requests.get(product_URL, headers=headers)
 
-title = soup.find("h1").text.strip()
-print("The title is", title)
+    soup = BeautifulSoup(page.content, 'html.parser') #Parses through all of the amazon HTML data
 
-price = soup.find("p", {"class":"price_color"}).text.strip()
-print("The price is", price)
+    price = soup.find("p", {"class":"price_color"}).text.strip()
+    price_num = float(price[1:])
 
-
+    print("The price is", price)
+    return price_num
